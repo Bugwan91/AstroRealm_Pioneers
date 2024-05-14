dec27f26bab54560cfe0bc6d092d6b10de849bce
Roman Movchan <roman.movchan@protonmail.com>
2024-05-14 03:09:37 +0300

    Updated plane grid: more visible when camera is hight

--- Assets/Code/GridPlane/Mono/GridPlaneMono.cs
+++ Assets/Code/GridPlane/Mono/GridPlaneMono.cs
@@ -8,16 +8,32 @@ namespace Code.MainController
 {
     public class GridPlane : MonoBehaviour
     {
+        public Transform cameraPosition;
+        public float minRadius = 500f;
+        public float maxRadius = 5000f;
+        public float minIntensity = 0f;
+        public float maxIntensity = 0.1f;
+        public float heightOfMaxIntensity = 5000f;
         
         private Material _material;
         private static readonly int Offset = Shader.PropertyToID("_Offset");
+        private static readonly int Intensity = Shader.PropertyToID("_Alpha");
         
         private EntityManager _entityManager;
         private Entity _floatingOrigin;
+
+        private float _minScale;
+        private float _maxScale;
+        private float _scaleDelta;
+        private float _intensityDelta;
         
         private void OnEnable()
         {
             _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            _minScale = minRadius * 0.1f;
+            _maxScale = maxRadius * 0.1f;
+            _scaleDelta = _maxScale - _minScale;
+            _intensityDelta = maxIntensity - minIntensity;
         }
         
         private void Start() {
@@ -32,6 +48,12 @@ namespace Code.MainController
             position.y = -1f;
             transform.position = position;
             _material.SetVector(Offset, -(Vector3)_entityManager.GetAspect<FloatingOriginAspect>(GetFOEntity()).TotalPosition);
+
+            var h = Mathf.Clamp(cameraPosition.position.y / heightOfMaxIntensity, 0f, 1f);
+            var scale = h * _scaleDelta + _minScale;
+            transform.localScale = new Vector3(scale, 1f, scale);
+            
+            _material.SetFloat(Intensity, h * _intensityDelta + minIntensity);
         }
         
         private Entity GetFOEntity()
